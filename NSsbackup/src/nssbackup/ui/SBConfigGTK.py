386934ffa3d49574ba99f90a5ed82dd1e5972342
Oumar Aziz OUATTARA (alias wattazoum) <wattazoum@gmail.com>
2007-10-21 08:22:28 +0200

    fixed version in about windows (to 0.1-2 )

--- NSsbackup/src/nssbackup/ui/SBConfigGTK.py
+++ NSsbackup/src/nssbackup/ui/SBConfigGTK.py
@@ -679,7 +679,8 @@ class SBconfigGTK(GladeWindow):
 	def on_about_activate(self, *args):
 		about = gtk.AboutDialog()
 		about.set_name("Not So Simple Backup Suite")
-		about.set_version("0.1-1")
+		# TODO: Always keep this updated
+		about.set_version("0.1-2")
 		about.set_comments(_("This is a user friendly backup solution for common desktop needs."))
 		about.set_transient_for(self.widgets["backup_properties_dialog"])
 		about.set_copyright("Oumar Aziz Ouattara <wattazoum@gmail.com>")
