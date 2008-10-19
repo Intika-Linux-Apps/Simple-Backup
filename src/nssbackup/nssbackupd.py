94a009ae51009fc81796035d1e98e0d9b9de4cb8
Oumar Aziz Ouattara (alias wattazoum) <wattazoum@gmail.com>
2008-10-19 17:53:51 +0200

    Change the format of the mail sent to be compliant with the standards (Fix LP #285927)

--- src/nssbackup/nssbackupd.py
+++ src/nssbackup/nssbackupd.py
@@ -17,6 +17,8 @@
 import os
 import traceback
 import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 import socket
 import datetime
 import re
@@ -76,6 +78,17 @@ class NSsbackupd () :
 				_content = _("I didn't find the log file. Please set it up in nssbackup.conf ")
 		
 		server = smtplib.SMTP()
+		msg = MIMEMultipart()
+		
+		msg['Subject'] = _title
+		msg['From'] = _from
+		msg['To'] = _to
+		msg.preamble = _title
+		
+		msg_content = MIMEText(_content)
+		# Set the filename parameter
+		msg_content.add_header('Content-Disposition', 'attachment', filename="nssbackup.log")
+		msg.attach(msg_content)
 		
 		# getting the connection
 		if self.__bm.config.has_option("report","smtpserver") :
@@ -92,9 +105,7 @@ class NSsbackupd () :
 			server.login(self.__bm.config.get("report","smtpuser"), self.__bm.config.get("report","smtppassword"))
 		
 		# send and close connection
-		_subject = "SUBJECT : %s \r\n" % _title
-		_header = "From : %s \r\nTo : %s \r\n" % (_from, _to)
-		server.sendmail(_from, _to, _header+_subject+_content)
+		server.sendmail(_from, _to, msg.as_string())
 		server.close()
 	
 		
