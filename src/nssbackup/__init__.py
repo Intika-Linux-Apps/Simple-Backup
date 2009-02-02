723fd80b067138ea1842534dad856b6b82c10ce0
Jean-Peer Lorenz <peer.loz@gmx.net>
2009-02-02 22:58:59 +0100

    * [FIX] re-base was not working due to errorneous merging of snapshot files 	and missing variable assignment -> fixed * [FIX] converting an incremental snapshot to a full one was errorneous: base 	references of child snapshots was not updated with *.ful name -> fixed * some doc-strings improved (using reST) * code clean-up and formatting while testing it

--- src/nssbackup/__init__.py
+++ src/nssbackup/__init__.py
@@ -1,13 +1,54 @@
+#	NSsbackup - the nssbackup package
+#
+#   Copyright (c)2007-2008: Ouattara Oumar Aziz <wattazoum@gmail.com>
+#   Copyright (c)2008-2009: Jean-Peer Lorenz <peer.loz@gmx.net>
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+"""
+:mod:`nssbackup-package` -- the nssbackup package
+==================================================
+
+.. module:: nssbackup-package
+   :synopsis: The main package
+.. moduleauthor:: Ouattara Oumar Aziz (alias wattazoum) <wattazoum@gmail.com>
+.. moduleauthor:: Jean-Peer Lorenz <peer.loz@gmx.net>
+
+"""
+
+
 import os
 import socket
 from gettext import gettext as _
 
-class Infos :
+class Infos(object):
     """
+    Collects general informations about NSsbackup.
+    
+    `name` The application's full name
+    
+	`version` Full current version
+	
+	`description` some description
+
     """
     
     NAME = "Not So Simple Backup Suite"
+
     VERSION = "0.2-0 RC2"
+    
     DESCRIPTION = _("This is a user friendly backup solution for common desktop needs.")
     WEBSITE = "https://launchpad.net/nssbackup/"
     
--- src/nssbackup/util/exceptions.py
+++ src/nssbackup/util/exceptions.py
@@ -1,24 +1,38 @@
-#    This program is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; either version 3 of the License, or
-#    (at your option) any later version.
+#	NSsbackup - custom exceptions
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+#   Copyright (c)2007-2008: Ouattara Oumar Aziz <wattazoum@gmail.com>
+#   Copyright (c)2008-2009: Jean-Peer Lorenz <peer.loz@gmx.net>
 #
-#    You should have received a copy of the GNU General Public License
-#    along with this program; if not, write to the Free Software
-#    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#
+"""
+:mod:`exceptions` -- custom exceptions
+======================================
 
-# Authors :
-#	Ouattara Oumar Aziz ( alias wattazoum ) <wattazoum@gmail.com>
+.. module:: util.exceptions
+   :synopsis: Defines custom exceptions
+.. moduleauthor:: Ouattara Oumar Aziz (alias wattazoum) <wattazoum@gmail.com>
+.. moduleauthor:: Jean-Peer Lorenz <peer.loz@gmx.net>
 
-class SBException(Exception) :
-	"""
-	This class will help us distinguish Exception that we must handle( Exception created by us) 
-	from programing errors Exception.
+"""
+
+class SBException(Exception):
+	"""This class will help us distinguish Exception that we must
+	handle (exceptions created by ourself) and exceptions due to 
+	programming errors (Python Exceptions).
+
 	"""
 
 class NotValidSnapshotException(SBException) :
@@ -27,42 +41,54 @@ class NotValidSnapshotException(SBException) :
 	"""
 	
 class NotValidSnapshotNameException(NotValidSnapshotException):
-	"""
-	Exception launched when the name of a snapshot is not valid
+	"""Exception launched when the name of a snapshot is not valid
+	
 	"""
 	
 	
 class NotValidSectionException(SBException) :
-	"""
-	This Exception is thrown by Config Section validation.
+	"""This Exception is thrown by Config Section validation.
+	
 	"""
 
 class NonValidOptionException(SBException):
-	"""
-	Thrown when a config option is not Valid
+	"""Thrown when a config option is not Valid
+
 	"""
 
 class CorruptedSBdictException(SBException):
-	"""
-	Thrown when a SBdict is corrupted
+	"""Thrown when a SBdict is corrupted
+
 	"""
 	
 class FuseFAMException(SBException):
-	"""
-	Thrown when a Fuse mount fails
+	"""Thrown when a Fuse mount fails
+
 	"""
 	
 class RebaseSnpException(SBException):
-	"""
-	Thrown for rebase exception
+	"""Thrown for rebase exception
+	
 	"""
 
 class RebaseFullSnpForbidden(RebaseSnpException):
+	"""Thrown when trying to rebase a full snapshot
+
 	"""
-	Thrown when trying to rebase a full snapshot
+
+class RemoveFullSnpForbidden(RebaseSnpException):
+	"""Thrown when trying to remove a full snapshot.
+	:todo: Check whether this exception is unused and can be removed!
+	
 	"""
 	
 class ChmodNotSupportedError(SBException):
 	"""Thrown when a destination does not support file modes (e.g. some
 	ftp servers or FAT filesystems).
+	
+	"""
+	
+class NotSupportedError(Exception):
+	"""Thrown when trying to call a stub.
+
 	"""
