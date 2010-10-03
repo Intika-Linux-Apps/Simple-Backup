a90931bbfea60eb7f76ad29a33a188e683020c3b
Jean-Peer Lorenz <peer.loz@gmx.net>
2010-10-03 16:32:50 +0200

    * Removed workaround for LP #640034 Setting status to attention does not change to specified icon (indicator-application): is now fixed in Maverick

--- src/sbackup/ui/indicator.py
+++ src/sbackup/ui/indicator.py
@@ -83,9 +83,6 @@ class SBackupdIndicator(SBackupdIndicatorBase, PyNotifyMixin):
 
     def set_status_to_attention(self):
         self._indicator.set_status(appindicator.STATUS_ATTENTION)
-        # Workaround for LP #640034:
-        # Setting status to attention does not change to specified icon (indicator-application)        
-        self._indicator.set_icon(constants.INDICATOR_ATTENTION_ICON)
 
     def set_status_to_finished(self):
         self._indicator.set_icon(constants.INDICATOR_SUCCESS_ICON)
